08b2891c944645b12b2b577cd7853b9dc1a544ff
MatthewKogo1 <matthewkogo1@gmail.com>
2019-11-13 00:07:29 +0300

    Added Live Chat

---
+++ web_accesibility.php
@@ -0,0 +1,7 @@
+<!--End of Tawk.to Script-->
+<script type="text/javascript">
+var _userway_config = {
+account: 'EiPLo35ion'
+};
+</script>
+<script type="text/javascript" src="https://cdn.userway.org/widget.js"></script>
