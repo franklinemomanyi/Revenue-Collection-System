8354f4096eb3fbe6a3e02bd14f76f54cd20d2ecd
Frankline Momanyi <39469432+franklinemomanyi@users.noreply.github.com>
2021-01-07 16:25:54 +0300

    Create Xxxxxx.php

---
+++ Xxxxxx.php
@@ -0,0 +1,196 @@
+<?php
+include('conn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Home</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- IonIcons -->
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/Adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to to the body tag
+to get the desired effect
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+<body class="hold-transition sidebar-mini layout-navbar-fixed layout-footer-fixed sidebar-collapse">
+<div class="wrapper">
+  <!-- Navbar -->
+  
+
+ <?php 
+ require('nav.php');
+ ?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+ <?php 
+ require('aside.php');
+ ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Admin Line Graph.</h1>
+          </div><!-- /.col -->
+         
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12">
+          
+            <div class="card">
+              
+              <div class="card-body">
+                <div class="d-flex">
+                  <p class="d-flex flex-column">
+                    <span class="text-bold text-lg">Admin Statistics</span>
+                    <span>Frankline O. Momanyi</span>
+                  </p>
+                  <!-- <p class="ml-auto d-flex flex-column text-right">
+                    <span class="text-success">
+                      <i class="fas fa-arrow-up"></i> 33.1%
+                    </span>
+                    <span class="text-muted">Since last month</span>
+                  </p> -->
+                </div>
+                <!-- /.d-flex -->
+
+                <div class="position-relative mb-4">
+                  <canvas id="bar-chart-grouped" height="100"></canvas>
+                </div>
+
+                <!-- <div class="d-flex flex-row justify-content-end">
+                  <span class="mr-2">
+                    <i class="fas fa-square text-primary"></i> Collections
+                  </span>
+
+                  <span>
+                    <i class="fas fa-square text-green"></i> Disbursement
+                  </span>
+                </div> -->
+              </div>
+            </div>
+            <!-- /.card -->
+
+
+           
+
+          </div>
+          <!-- /.col-md-6 -->
+          
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+   <?php 
+   require('footer.php');
+   ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="dist/js/Adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="plugins/chart.js/javascript.js"></script>
+<script src="dist/js/demo.js"></script>
+<script src="dist/js/pages/dashboard3.js"></script>
+
+<script type="text/javascript">
+
+  new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'line',
+    data: {
+      labels: ["Jan", "Feb", "Mar", "Apr","May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec"],
+      datasets: [
+        {
+          label: "Wins",
+          backgroundColor: "#0047b3",
+          data: [15,30,9,23,37,41,46,53,54,47,69,72],
+          fill: false
+        }, {
+          label: "Loses",
+          backgroundColor: "#009999",
+          data: [3,5,1,6,10,2,3,4,2,5,2,1],
+          fill: false
+        }
+      ]
+    },
+    options: {
+      // title: {
+      //   display: false,
+      //   text: 'Population growth (millions)'
+      // }
+
+      scales: {
+        xAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          }
+        }],
+
+        yAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          }
+        }],
+      },
+      legend: { display: true,},
+      
+
+
+    }
+});
+</script>
+
+
+</body>
+</html>
